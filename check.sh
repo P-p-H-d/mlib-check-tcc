7087c5d8709ec39277fc14824b1c7ef8fbfbe0f3
Patrick Pelissier <Patrick.Pelissier@gmail.com>
2023-12-22 16:16:53 +0100

    Test segafault of m-worker

---
+++ check.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+MAX=10
+ok=0
+
+function run_test
+{
+    CC="$1"
+    ok=0
+    for i in $(seq 1 $MAX) ; do
+        cd mlib/tests
+        make clean
+        make CC="$CC -DM_USE_THREAD_BACKEND=3" test-mworker.log
+        test $? -eq 0 && ok=$(($ok + 1))
+        cd ../..
+    done
+    echo $ok
+}
+
+git clone git@github.com:P-p-H-d/mlib.git
+cp ./m-atomic.h mlib/
+
+run_test gcc
+ok_gcc=$ok
+run_test clang
+ok_clang=$ok
+run_test tcc
+ok_tcc=$ok
+
+echo "RESULT:"
+echo "GCC  : $ok_gcc / $MAX"
+echo "CLANG: $ok_clang / $MAX"
+echo "TCC  : $ok_tcc / $MAX"
+exit 0
